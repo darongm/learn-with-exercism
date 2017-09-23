b64bfad8c822603679b0798fafd2210b569d90ea
Darong Mean <darong012@gmail.com>
2017-09-23 16:08:24 +0700

    refactor
    inspired by the following:
    - http://exercism.io/submissions/0c59db1ef0b64b1cb1ad68088fdbf4f5
    - http://exercism.io/submissions/f804b2fc21c74a48bcbdfdb881ed4224
    - http://exercism.io/submissions/21d2366a29b84d63bd05ecca5290be09

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -2,23 +2,27 @@
   (:require
    [clojure.string :as string]))
 
-(defn letter? [^Character c]
-  (Character/isLetter c))
 
-(defn upper-case? [^Character c]
-  (Character/isUpperCase c))
+(defn yell? [param]
+  (let [letter?     (fn [^Character c] (Character/isLetter c))
+        upper-case? (fn [^Character c] (Character/isUpperCase c))]
+    (some->> param
+      (filter letter?)
+      (not-empty) ; this is required to work with some->>, since (every? true? []) always return true.
+      (every? upper-case?))))
+
+
+(defn question? [param]
+  (string/ends-with? param "?"))
+
+
+(defn silence? [param]
+  (string/blank? param))
+
 
 (defn response-for [param]
-  (let [yell?     (some->> param
-                    (filter letter?)
-                    (seq) ; this is required to work with some->>, since (every? true? []) always return true.
-                    (every? upper-case?))
-        question? (string/ends-with? param "?")
-        silence?  (->> param
-                    (string/trim)
-                    (empty?))]
-    (cond
-      silence? "Fine. Be that way!"
-      yell? "Whoa, chill out!"
-      question? "Sure."
-      :else "Whatever.")))
+  (cond
+    (silence? param) "Fine. Be that way!"
+    (yell? param) "Whoa, chill out!"
+    (question? param) "Sure."
+    :else "Whatever."))
