0fd0144f8d5b33c274e7282b32e8e19932b664a1
Darong Mean <darong012@gmail.com>
2017-09-23 17:25:34 +0700

    refactor

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,14 +1,18 @@
 (ns rna-transcription)
 
 
-(def dna->rna {\C \G
-               \G \C
-               \A \U
-               \T \A})
+(def dna-rna-map {\C \G
+                  \G \C
+                  \A \U
+                  \T \A})
 
 
-(defn to-rna [nucleotides]
-  {:pre [(every? dna->rna nucleotides)]}
-  (->> nucleotides
+(defn dna->rna [n]
+  {:pre [(contains? dna-rna-map n)]}
+  (get dna-rna-map n))
+
+
+(defn to-rna [dna]
+  (->> dna
     (map dna->rna)
     (apply str)))
