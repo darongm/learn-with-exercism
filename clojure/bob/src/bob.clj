dc4740b3a2cb970d846dd760f827a57fc6c9dd93
Darong Mean <darong012@gmail.com>
2017-09-23 15:39:12 +0700

    solution to problem 2

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,5 +1,24 @@
-(ns bob)
+(ns bob
+  (:require
+   [clojure.string :as string]))
 
-(defn response-for [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn letter? [^Character c]
+  (Character/isLetter c))
+
+(defn upper-case? [^Character c]
+  (Character/isUpperCase c))
+
+(defn response-for [param]
+  (let [yell?     (some->> param
+                    (filter letter?)
+                    (seq)
+                    (every? upper-case?))
+        question? (string/ends-with? param "?")
+        silence?  (->> param
+                    (string/trim)
+                    (empty?))]
+    (cond
+      silence? "Fine. Be that way!"
+      yell? "Whoa, chill out!"
+      question? "Sure."
+      :else "Whatever.")))
