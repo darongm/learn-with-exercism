272e0ac5cd12597d761a2c43b88e518d0ea52c00
Darong Mean <darong012@gmail.com>
2017-09-24 13:09:45 +0700

    `frequencies` can use with string since string is a collection of characters

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -3,13 +3,13 @@
    [clojure.string :as string]))
 
 
+;; inspired by http://exercism.io/submissions/1f62653037ae47da9fe8fd9d2915d090
 (defn anagram? [word1 word2]
-  (let [lc-word1    (string/lower-case word1)
-        lc-word2    (string/lower-case word2)
-        letter-coll #(->> % vec frequencies)]
+  (let [lc-word1 (string/lower-case word1)
+        lc-word2 (string/lower-case word2)]
     (and
       (not= lc-word1 lc-word2)
-      (= (letter-coll lc-word1) (letter-coll lc-word2)))))
+      (= (frequencies lc-word1) (frequencies lc-word2)))))
 
 
 (defn anagrams-for [word coll]
