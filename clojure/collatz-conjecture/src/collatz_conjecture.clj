5e7ada27f431586f1659d892d1bbefc40098d709
Darong Mean <darong012@gmail.com>
2017-09-24 15:39:45 +0700

    solution to problem 6

--- clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -1,3 +1,20 @@
 (ns collatz-conjecture)
 
-(defn collatz [n])
+
+(defn apply-math [n]
+  (if (even? n)
+    (/ n 2)
+    (+ (* 3 n) 1)))
+
+
+(defn collatz- [n]
+  (->> n
+    (iterate apply-math)
+    (take-while #(not= 1 %))
+    (count)))
+
+
+(defn collatz [n]
+  (if (< n 1)
+    (throw (IllegalArgumentException. "zero or negative value is an error"))
+    (collatz- n)))
