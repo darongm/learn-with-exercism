b7587b7479b10dc1eb1a1990ad4d075e2d15e672
Darong Mean <darong012@gmail.com>
2017-09-24 13:19:44 +0700

    due to equality semantics of sequence, no need to return vector

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -3,7 +3,9 @@
    [clojure.string :as string]))
 
 
-;; inspired by http://exercism.io/submissions/1f62653037ae47da9fe8fd9d2915d090
+;; inspired by
+;; - http://exercism.io/submissions/1f62653037ae47da9fe8fd9d2915d090
+;; - http://exercism.io/submissions/1c028cf67aec45ed9e48e5bdb47e32c2
 (defn anagram? [word1 word2]
   (let [lc-word1 (string/lower-case word1)
         lc-word2 (string/lower-case word2)]
@@ -13,4 +15,4 @@
 
 
 (defn anagrams-for [word coll]
-  (filterv #(anagram? word %) coll))
+  (filter #(anagram? word %) coll))
