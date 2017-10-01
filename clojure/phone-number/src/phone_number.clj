e7c89bae6817b37b48b7d34cf24238b687673c24
Darong Mean <darong012@gmail.com>
2017-10-01 19:11:21 +0700

    refactor code to be more readable

--- clojure/phone-number/src/phone_number.clj
+++ clojure/phone-number/src/phone_number.clj
@@ -3,35 +3,37 @@
 
 
 (defn nanp-number [digits]
-  (let [[ac1 ac2 ac3 ec1 ec2 ec3 & sn] digits]
-    [[ac1 ac2 ac3] [ec1 ec2 ec3] sn]))
+  (let [[ac1 ac2 ac3 ec1 ec2 ec3 sn1 sn2 sn3 sn4] digits]
+    [(str ac1 ac2 ac3)
+     (str ec1 ec2 ec3)
+     (str sn1 sn2 sn3 sn4)]))
 
 
 (defn ->nanp-number [coll]
   (let [digit? (fn [^Character c] (Character/isDigit c))
         digits (filter digit? coll)]
     (cond
-      (= 10 (count digits)) (nanp-number digits)
-      (= [11 \1] [(count digits) (first digits)]) (nanp-number (rest digits))
-      :else [[\0 \0 \0] [\0 \0 \0] [\0 \0 \0 \0]])))
+      (= 10 (count digits))
+      (nanp-number digits)
+
+      (and (= 11 (count digits)) (= \1 (first digits)))
+      (nanp-number (rest digits))
+
+      :else ["000" "000" "0000"])))
 
 
 (defn number [coll]
   (->> coll
     (->nanp-number)
-    (flatten)
     (string/join)))
 
 
 (defn area-code [coll]
-  (let [[area-code _ _] (->nanp-number coll)]
-    (string/join area-code)))
+  (->> coll
+    (->nanp-number)
+    (first)))
 
 
 (defn pretty-print [coll]
   (let [[area-code exchange-code subscriber-number] (->nanp-number coll)]
-    (str
-      "(" (string/join area-code) ") "
-      (string/join exchange-code)
-      "-"
-      (string/join subscriber-number))))
+    (format "(%s) %s-%s" area-code exchange-code subscriber-number)))
