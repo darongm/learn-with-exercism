8058f87aead17c0e344b84215f3e40b56f7f9c62
Darong Mean <darong012@gmail.com>
2017-10-15 12:54:56 +0700

    implement add function

--- clojure/grade-school/src/grade_school.clj
+++ clojure/grade-school/src/grade_school.clj
@@ -1,7 +1,9 @@
 (ns grade-school)
 
 
-(defn add [db name grade])
+(defn add [db student-name student-grade]
+  (let [conj+ (fnil conj [])]
+    (update-in db [student-grade] conj+ student-name)))
 
 
 (defn grade [db grade])
