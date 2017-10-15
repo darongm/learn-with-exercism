6960fe09170e7a0db12b27a3f239d1a400cd0bdf
Darong Mean <darong012@gmail.com>
2017-10-15 13:57:58 +0700

    solution for problem 12

--- clojure/grade-school/src/grade_school.clj
+++ clojure/grade-school/src/grade_school.clj
@@ -1,17 +1,27 @@
 (ns grade-school)
 
 
+(comment
+  (defn add [db student-name student-grade]
+    (let [conj+ (fnil conj [])]
+      (update-in db [student-grade] conj+ student-name))))
+
+
+;; inspired by http://exercism.io/submissions/4a3c941574f04eebbe5982481c0ed127
+;; and http://clojuredocs.org/clojure.core/merge-with#example-5653839de4b0be225c0c4799
 (defn add [db student-name student-grade]
-  (let [conj+ (fnil conj [])]
-    (update-in db [student-grade] conj+ student-name)))
+  (merge-with into db {student-grade [student-name]}))
 
 
 (defn grade [db grade]
   (get db grade []))
 
 
+;; inspired by http://exercism.io/submissions/837c8fb14cb6466bb3d18fafc87b21b5
+;; to preserve vector after sort
 (defn sorted [db]
-  (let [sorted-names (fn [[g names]] [g (sort names)])]
+  (let [sort-v       (fn [v] (into [] (sort v)))
+        sorted-names (fn [[g names]] [g (sort-v names)])]
     (->> db
       (map sorted-names)
       (into (sorted-map-by <)))))
