1785a8c17e29c98ea6ed8fcec28337e50f78c167
Darong Mean <darong012@gmail.com>
2017-10-22 13:59:27 +0700

    first attempt

--- clojure/robot-name/src/robot_name.clj
+++ clojure/robot-name/src/robot_name.clj
@@ -1,10 +1,32 @@
-(ns robot-name)
+(ns robot-name
+ (:require
+  [clojure.string :as string]))
 
 
-(defn robot [])
+(defn random-robot []
+  (let [asci-letters (range 65 91)
+        asci-digits  (range 48 57)
+        part1        (take 2 (shuffle asci-letters))
+        part2        (take 3 (shuffle asci-digits))
+        full-name    (map char (concat part1 part2))]
+    (string/join full-name)))
 
 
-(defn robot-name [robot])
+(defn -robot
+  ([]
+   (random-robot))
+  ([old-name]
+   (->> (repeatedly random-robot)
+     (remove #{old-name})
+     (first))))
 
 
-(defn reset-name [robot])
+(defn robot []
+  (atom (-robot)))
+
+
+(defn robot-name [*robot] @*robot)
+
+
+(defn reset-name [*robot]
+  (swap! *robot -robot))
