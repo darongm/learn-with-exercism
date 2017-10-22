db1d2e042c8380ba3f13a95281aed905fe39488b
Darong Mean <darong012@gmail.com>
2017-10-22 15:01:44 +0700

    add some document

--- clojure/robot-name/src/robot_name.clj
+++ clojure/robot-name/src/robot_name.clj
@@ -12,6 +12,7 @@
     (string/join full-name)))
 
 
+;;; doc https://clojure.org/reference/refs
 (def *robot-coll (ref []))
 
 
@@ -21,6 +22,20 @@
               (remove #(contains? coll %))
               (first)
               (conj coll)))]
+    ; If use `atom` like below, there's a race condition.
+    ; Assuming the method called by 2 callers at the same time. The following can happen:
+    ; Caller 1: (swap! *robot-coll f) -> ["123"]
+    ; Caller 2: (swap! *robot-coll f) -> ["123" "456"]
+    ; Caller 1: (last @*robot-coll)-> "456"
+    ; Caller 2: (last @*robot-coll)-> "456"
+    ; Which is wrong cause `Caller 1` expects "123" and the names are not unique.
+    (comment
+      (do
+        (swap! *robot-coll f)
+        (last @*robot-coll)))
+    ; Using the semantics of `ref`, changes to `*robot-coll` and `*robot`
+    ; are done in one transaction.
+    ; The trade off is that `ref` reduce concurrency executions.
     (dosync
       (alter *robot-coll f)
       (ref-set *robot (last @*robot-coll)))))
