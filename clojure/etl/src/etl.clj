0c48b234d7f67bd984b50c29737cf8f2b07f1bfe
Darong Mean <darong012@gmail.com>
2017-10-28 20:16:26 +0700

    second solution

--- clojure/etl/src/etl.clj
+++ clojure/etl/src/etl.clj
@@ -4,4 +4,9 @@
 (defn transform [m]
   (let [f (fn [acc [k coll]]
             (reduce #(assoc %1 (clojure.string/lower-case %2) k) acc coll))]
-    (reduce f {} m)))
+    (->> m
+      (map (fn [[score letter-coll]] (zipmap letter-coll (repeat score))))
+      (reduce merge {})
+      (reduce-kv #(assoc %1 (clojure.string/lower-case %2) %3) {}))))
+
+
