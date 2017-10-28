de6760ad69b542f0ce3c88fc0084672b162cd640
Darong Mean <darong012@gmail.com>
2017-10-28 19:48:31 +0700

    first solution

--- clojure/etl/src/etl.clj
+++ clojure/etl/src/etl.clj
@@ -1,4 +1,7 @@
 (ns etl)
 
 
-(defn transform [m])
+(defn transform [m]
+  (let [f (fn [acc [k coll]]
+            (reduce #(assoc %1 (clojure.string/lower-case %2) k) acc coll))]
+    (reduce f {} m)))
