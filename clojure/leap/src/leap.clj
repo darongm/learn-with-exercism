8fcc1980fc0203c7ac9ab6c870443ce385bffbec
Darong Mean <darong012@gmail.com>
2017-10-28 18:24:37 +0700

    first solution

--- clojure/leap/src/leap.clj
+++ clojure/leap/src/leap.clj
@@ -1,4 +1,8 @@
 (ns leap)
 
 
-(defn leap-year? [n])
+(defn leap-year? [n]
+  (let [db4   (= 0 (mod n 4))
+        db100 (= 0 (mod n 100))
+        db400 (= 0 (mod n 400))]
+    (or db400 (and db4 (not db100)))))
