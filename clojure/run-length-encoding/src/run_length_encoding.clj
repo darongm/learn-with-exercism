4e74c2011b583a93a609639a9c974ce5f4ae92cb
Darong Mean <darong012@gmail.com>
2017-11-04 14:58:41 +0700

    implement encode

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -1,11 +1,22 @@
-(ns run-length-encoding)
+(ns run-length-encoding
+  (:require
+   [clojure.string :as string]
+   [clojure.set :as sets]))
+
 
 (defn run-length-encode
   "encodes a string with run-length-encoding"
   [s]
-  )
+  (let [ret (partition-by identity s)
+        ret (map frequencies ret)
+        ret (map sets/map-invert ret)
+        ret (mapcat vec ret)
+        ret (reduce concat ret)
+        ret (remove #(= 1 %) ret)
+        ret (string/join ret)]
+    ret))
+
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
-  [s]
-  )
+  [s])
