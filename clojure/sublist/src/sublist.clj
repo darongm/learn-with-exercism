84551ab835f8b3a3b419754b10b267e3893a6236
Darong Mean <darong012@gmail.com>
2017-11-11 20:22:36 +0700

    first solution

--- clojure/sublist/src/sublist.clj
+++ clojure/sublist/src/sublist.clj
@@ -1,4 +1,14 @@
 (ns sublist)
 
 
-(defn classify [coll1 coll2])
+(defn sublist? [a-coll b-coll]
+  (let [candidates (partition (count a-coll) 1 b-coll)]
+    (some #{a-coll} candidates)))
+
+
+(defn classify [a-coll b-coll]
+  (cond
+    (= a-coll b-coll) :equal
+    (sublist? a-coll b-coll) :sublist
+    (sublist? b-coll a-coll) :superlist
+    :else :unequal))
