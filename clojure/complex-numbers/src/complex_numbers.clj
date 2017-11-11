c2f22a2da4396a85192ba81fe49f2f8eb9224fa0
Darong Mean <darong012@gmail.com>
2017-11-11 18:44:56 +0700

    solution

--- clojure/complex-numbers/src/complex_numbers.clj
+++ clojure/complex-numbers/src/complex_numbers.clj
@@ -1,17 +1,25 @@
 (ns complex-numbers)
 
-(defn real [[a b]])
+(defn real [[a b]] a)
 
-(defn imaginary [[a b]])
+(defn imaginary [[a b]] b)
 
-(defn abs [[a b]])
+(defn abs [[a b]]
+  (Math/sqrt (+ (* a a) (* b b))))
 
-(defn conjugate [[a b]])
+(defn conjugate [[a b]]
+  [a (- b)])
 
-(defn add [[a b] [c d]])
+(defn add [[a b] [c d]]
+  [(+ a c) (+ b d)])
 
-(defn sub [[a b] [c d]])
+(defn sub [[a b] [c d]]
+  [(- a c) (- b d)])
 
-(defn mul [[a b] [c d]])
+(defn mul [[a b] [c d]]
+  [(- (* a c) (* b d))
+   (+ (* b c) (* a d))])
 
-(defn div [[a b] [c d]])
+(defn div [[a b] [c d]]
+  [(double (/ (+ (* a c) (* b d)) (+ (* c c) (* d d))))
+   (double (/ (- (* b c) (* a d)) (+ (* c c) (* d d))))])
