4cd79ae8cfa2d5f14d5e5b44aaaf6aeee56a0b48
Darong Mean <darong012@gmail.com>
2017-11-11 20:55:46 +0700

    cheat solution with Java binary search

--- clojure/binary-search/src/binary_search.clj
+++ clojure/binary-search/src/binary_search.clj
@@ -1,7 +1,18 @@
-(ns binary-search)
+(ns binary-search
+  (:import
+   [java.util Collections]))
 
 
-(defn middle [coll])
+(defn middle [coll]
+  (quot (count coll) 2))
 
 
-(defn search-for [n coll])
+(defn jbinary-search [n coll]
+  (Collections/binarySearch coll n))
+
+
+(defn search-for [n coll]
+  (let [idx (jbinary-search n coll)]
+    (if (> idx -1)
+      idx
+      (throw (Exception. "not found")))))
