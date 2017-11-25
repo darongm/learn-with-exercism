cedc40f6321d9813fe63cc4edba01dafab0363b9
Darong Mean <darong012@gmail.com>
2017-11-25 13:39:26 +0700

    solution

--- clojure/perfect-numbers/src/perfect_numbers.clj
+++ clojure/perfect-numbers/src/perfect_numbers.clj
@@ -1,4 +1,16 @@
 (ns perfect-numbers)
 
 
-(defn classify [n])
+(defn classify- [n]
+  (let [factors (->> n (range 1) (filter #(= 0 (mod n %))))
+        sum     (reduce + factors)]
+    (cond
+      (> n sum) :deficient
+      (< n sum) :abundant
+      :else :perfect)))
+
+
+(defn classify [n]
+  (if (< n 0)
+    (throw (IllegalArgumentException.))
+    (classify- n)))
