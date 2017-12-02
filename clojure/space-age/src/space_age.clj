22a0d1e46723cb5fb5ea0ec67c0a151217702d18
Darong Mean <darong012@gmail.com>
2017-12-02 19:14:22 +0700

    add another solution

--- clojure/space-age/src/space_age.clj
+++ clojure/space-age/src/space_age.clj
@@ -23,3 +23,22 @@
 
 (defn on-neptune [n]
   (/ (on-earth n) 164.79132))
+
+;; more elegant solution
+;; from http://exercism.io/submissions/18f6d12ebd2b48108d8dcffbf2688ab7
+(comment
+  (def earth-year 31557600)
+
+  (defn on-earth [sec]
+    (/ sec earth-year))
+
+  (defn- relative-to-earth [scale sec]
+    (/ (on-earth sec) scale))
+
+  (def on-mercury (partial relative-to-earth 0.2408465))
+  (def on-venus (partial relative-to-earth 0.61519726))
+  (def on-mars (partial relative-to-earth 1.8808158))
+  (def on-jupiter (partial relative-to-earth 11.862615))
+  (def on-saturn (partial relative-to-earth 29.447498))
+  (def on-uranus (partial relative-to-earth 84.016846))
+  (def on-neptune (partial relative-to-earth 164.79132)))
