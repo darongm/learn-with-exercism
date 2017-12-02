08a46256ff85d7d7fc6628bbb5751aee23475b77
Darong Mean <darong012@gmail.com>
2017-12-02 18:20:41 +0700

    first attempt solution

--- clojure/space-age/src/space_age.clj
+++ clojure/space-age/src/space_age.clj
@@ -1,17 +1,25 @@
 (ns space-age)
 
-(defn on-earth [n])
+(defn on-earth [n]
+  (/ n 31557600))
 
-(defn on-mercury [n])
+(defn on-mercury [n]
+  (/ (on-earth n) 0.2408467))
 
-(defn on-venus [n])
+(defn on-venus [n]
+  (/ (on-earth n) 0.61519726))
 
-(defn on-mars [n])
+(defn on-mars [n]
+  (/ (on-earth n) 1.8808158))
 
-(defn on-jupiter [n])
+(defn on-jupiter [n]
+  (/ (on-earth n) 11.862615))
 
-(defn on-saturn [n])
+(defn on-saturn [n]
+  (/ (on-earth n) 29.447498))
 
-(defn on-uranus [n])
+(defn on-uranus [n]
+  (/ (on-earth n) 84.016846))
 
-(defn on-neptune [n])
+(defn on-neptune [n]
+  (/ (on-earth n) 164.79132))
