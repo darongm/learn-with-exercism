870aa040ef39706edf512db856de9b6b33134aff
Darong Mean <darong012@gmail.com>
2017-12-02 19:36:27 +0700

    rename

--- clojure/grains/src/grains.clj
+++ clojure/grains/src/grains.clj
@@ -1,13 +1,13 @@
 (ns grains)
 
 
-(defn grain-seq [n]
-  (take n (iterate (partial * 2) (biginteger 1))))
+(defn take-power-of-2 [n]
+  (take n (iterate (partial * 2) BigInteger/ONE)))
 
 
 (defn square [n]
-  (last (grain-seq n)))
+  (last (take-power-of-2 n)))
 
 
 (defn total []
-  (reduce + (grain-seq 64)))
+  (reduce + (take-power-of-2 64)))
