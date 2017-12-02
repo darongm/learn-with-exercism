22fd3502542e80d1b7e38bc1627066c34df0b16b
Darong Mean <darong012@gmail.com>
2017-12-02 20:44:39 +0700

    first attempt

---
+++ clojure/gigasecond/src/gigasecond.clj
@@ -0,0 +1,11 @@
+(ns gigasecond
+  (:import
+   [java.time LocalDateTime]))
+
+
+(defn from [^Integer year ^Integer month ^Integer day]
+  (let [dt (LocalDateTime/of year month day 0 0 0)
+        dt (.plusSeconds dt 1000000000)]
+    [(.getYear dt)
+     (.getMonthValue dt)
+     (.getDayOfMonth dt)]))
