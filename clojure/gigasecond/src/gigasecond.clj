801a7698f651f3515c78a2f269206f53d146b799
Darong Mean <darong012@gmail.com>
2017-12-02 20:46:33 +0700

    a better way to write literal

--- clojure/gigasecond/src/gigasecond.clj
+++ clojure/gigasecond/src/gigasecond.clj
@@ -5,7 +5,7 @@
 
 (defn from [^Integer year ^Integer month ^Integer day]
   (let [dt (LocalDateTime/of year month day 0 0 0)
-        dt (.plusSeconds dt 1000000000)]
+        dt (.plusSeconds dt 1e9)]
     [(.getYear dt)
      (.getMonthValue dt)
      (.getDayOfMonth dt)]))
