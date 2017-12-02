1a1848e924d530d8ebbfb8a60c2425b882f45a2a
Darong Mean <darong012@gmail.com>
2017-12-02 18:05:09 +0700

    dummy namespace

---
+++ clojure/space-age/src/space_age.clj
@@ -0,0 +1,17 @@
+(ns space-age)
+
+(defn on-earth [n])
+
+(defn on-mercury [n])
+
+(defn on-venus [n])
+
+(defn on-mars [n])
+
+(defn on-jupiter [n])
+
+(defn on-saturn [n])
+
+(defn on-uranus [n])
+
+(defn on-neptune [n])
