829a3b3971c621834f336ad618a1470dcd72d1c8
Darong Mean <darong012@gmail.com>
2017-12-16 22:35:22 +0700

    first attempt

--- clojure/triangle/src/triangle.clj
+++ clojure/triangle/src/triangle.clj
@@ -1,4 +1,18 @@
 (ns triangle)
 
 
-(defn type [a b c])
+(defn type [a b c]
+  (cond
+    (or
+      (> 0 a b c)
+      (>= a (+ b c))
+      (>= b (+ a c))
+      (>= c (+ a b)))
+    :illogical
+
+    (= a b c) :equilateral
+
+    (or (= a b) (= a c) (= b c)) :isosceles
+
+    :else :scalene))
+
