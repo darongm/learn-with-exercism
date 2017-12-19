e78f5bca3f73eca60e732e97228c0137c93974dc
Darong Mean <darong012@gmail.com>
2017-12-19 22:26:05 +0700

    implement valid formula

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -1,3 +1,24 @@
 (ns isbn-verifier)
 
-(defn isbn? [isbn])
+
+(defn parse-isbn [isbn]
+  (->> isbn
+    (re-seq #"(\d)-(\d)(\d)(\d)-(\d)(\d)(\d)(\d)(\d)-(\d|X)")
+    (first)
+    (rest)))
+
+
+(defn valid-formula? [digits]
+  (let [mod-11     #(mod % 11)
+        factors    (range 1 (inc (count digits)))
+        int-digits (map #(Integer/parseInt %) digits)]
+    (->>
+      (map * int-digits factors)
+      (reduce +)
+      (mod-11)
+      (zero?))))
+
+
+(defn isbn? [isbn]
+  (let [digits (parse-isbn isbn)]
+    (valid-formula? digits)))
