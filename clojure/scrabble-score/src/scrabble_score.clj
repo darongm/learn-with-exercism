763321dcba42bdfacb3ce5c45ad7814690164b76
Darong Mean <darong012@gmail.com>
2017-12-25 20:55:45 +0700

    rename

--- clojure/scrabble-score/src/scrabble_score.clj
+++ clojure/scrabble-score/src/scrabble_score.clj
@@ -5,18 +5,18 @@
 
 (defn score-letter [letter]
   {:pre [(or (char? letter) (= 1 (count letter)))]}
-  (let [scores        {["a" "e" "i" "o" "u" "l" "n" "r" "s" "t"] 1
-                       ["d" "g"]                                 2
-                       ["b" "c" "m" "p"]                         3
-                       ["f" "h" "v" "w" "y"]                     4
-                       ["k"]                                     5
-                       ["j" "x"]                                 8
-                       ["q" "z"]                                 10}
-        letter->value (fn [k v] (zipmap k (repeat v)))
-        ->value       (reduce-kv #(merge %1 (letter->value %2 %3)) {} scores)]
-    (-> letter
+  (let [scores          {["a" "e" "i" "o" "u" "l" "n" "r" "s" "t"] 1
+                         ["d" "g"]                                 2
+                         ["b" "c" "m" "p"]                         3
+                         ["f" "h" "v" "w" "y"]                     4
+                         ["k"]                                     5
+                         ["j" "x"]                                 8
+                         ["q" "z"]                                 10}
+        letter->value   (fn [k v] (zipmap k (repeat v)))
+        score-by-letter (reduce-kv #(merge %1 (letter->value %2 %3)) {} scores)]
+    (->> letter
       (string/lower-case)
-      (->value))))
+      (get score-by-letter))))
 
 
 (defn score-word [word]
