948363a258d1271573a30a876ee26d30e2eee745
Darong Mean <darong012@gmail.com>
2017-12-25 20:38:22 +0700

    refactor to be more readable

--- clojure/scrabble-score/src/scrabble_score.clj
+++ clojure/scrabble-score/src/scrabble_score.clj
@@ -5,17 +5,18 @@
 
 (defn score-letter [letter]
   {:pre [(or (char? letter) (= 1 (count letter)))]}
-  (let [value->letter {1  ["a" "e" "i" "o" "u" "l" "n" "r" "s" "t"]
-                       2  ["d" "g"]
-                       3  ["b" "c" "m" "p"]
-                       4  ["f" "h" "v" "w" "y"]
-                       5  ["k"]
-                       8  ["j" "x"]
-                       10 ["q" "z"]}
-        letter->value (->> value->letter
-                        (mapcat (fn [[k v]] (zipmap v (repeat k))))
-                        (into (hash-map)))]
-    (-> letter (string/lower-case) (letter->value))))
+  (let [scores        {["a" "e" "i" "o" "u" "l" "n" "r" "s" "t"] 1
+                       ["d" "g"]                                 2
+                       ["b" "c" "m" "p"]                         3
+                       ["f" "h" "v" "w" "y"]                     4
+                       ["k"]                                     5
+                       ["j" "x"]                                 8
+                       ["q" "z"]                                 10}
+        letter->value (fn [k v] (zipmap k (repeat v)))
+        ->value       (reduce-kv #(merge %1 (letter->value %2 %3)) {} scores)]
+    (-> letter
+      (string/lower-case)
+      (->value))))
 
 
 (defn score-word [word]
