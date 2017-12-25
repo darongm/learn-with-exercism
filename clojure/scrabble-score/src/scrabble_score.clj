e37f5b7085e896835cf65c7691b2ed46669271c3
Darong Mean <darong012@gmail.com>
2017-12-25 22:11:40 +0700

    make function more readable

--- clojure/scrabble-score/src/scrabble_score.clj
+++ clojure/scrabble-score/src/scrabble_score.clj
@@ -3,21 +3,24 @@
    [clojure.string :as string]))
 
 
+;; inspired the hash map naming from http://www.lispcast.com/clojure-hashmaps
+(def score-by-letter
+  (let [scores        {["a" "e" "i" "o" "u" "l" "n" "r" "s" "t"] 1
+                       ["d" "g"]                                 2
+                       ["b" "c" "m" "p"]                         3
+                       ["f" "h" "v" "w" "y"]                     4
+                       ["k"]                                     5
+                       ["j" "x"]                                 8
+                       ["q" "z"]                                 10}
+        split-key-map (fn [ks v] (zipmap ks (repeat v)))]
+    (reduce-kv #(merge %1 (split-key-map %2 %3)) {} scores)))
+
+
 (defn score-letter [letter]
   {:pre [(or (char? letter) (= 1 (count letter)))]}
-  (let [scores          {["a" "e" "i" "o" "u" "l" "n" "r" "s" "t"] 1
-                         ["d" "g"]                                 2
-                         ["b" "c" "m" "p"]                         3
-                         ["f" "h" "v" "w" "y"]                     4
-                         ["k"]                                     5
-                         ["j" "x"]                                 8
-                         ["q" "z"]                                 10}
-        split-key-map   (fn [ks v] (zipmap ks (repeat v)))
-        score-by-letter (reduce-kv #(merge %1 (split-key-map %2 %3)) {} scores)]
-    (->> letter
-      (string/lower-case)
-      ; inspired the hash map naming from http://www.lispcast.com/clojure-hashmaps
-      (get score-by-letter))))
+  (->> letter
+    (string/lower-case)
+    (get score-by-letter)))
 
 
 (defn score-word [word]
