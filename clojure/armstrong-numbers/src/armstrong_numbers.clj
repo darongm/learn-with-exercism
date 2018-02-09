d1700eebb427de6b33b8b5ff99e602a19a82dea9
Darong Mean <darong012@gmail.com>
2018-02-09 22:55:24 +0700

    naive implementation

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,3 +1,6 @@
 (ns armstrong-numbers)
 
-(defn armstrong? [n])
+(defn armstrong? [n]
+  (let [digits (map #(Integer/parseInt (str %)) (str n))
+        ret (reduce + (map #(Math/pow %1 (count digits)) digits))]
+    (zero? (- n ret))))
