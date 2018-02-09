888acf961c8c70772b0602b269907f18b7f55270
Darong Mean <darong012@gmail.com>
2018-02-09 23:16:04 +0700

    use shorter function to convert Character to digit

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,7 +1,7 @@
 (ns armstrong-numbers)
 
 (defn armstrong? [n]
-  (let [digits (map #(Integer/parseInt (str %)) (str n))
+  (let [digits (map #(Character/getNumericValue ^Character %) (str n))
         digit-count (count digits)
         sum (->> digits (map #(Math/pow %1 digit-count)) (reduce +))]
     (zero? (- n sum))))
