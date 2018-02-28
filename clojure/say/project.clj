032476395d5e2daa6432c92e10396f432e3da3f4
Darong Mean <darong012@gmail.com>
2018-02-28 21:35:23 +0700

    add tool to auto run test

--- clojure/say/project.clj
+++ clojure/say/project.clj
@@ -2,4 +2,6 @@
   :description "say exercise."
   :url "https://github.com/exercism/clojure/tree/master/exercises/say"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/test.check "0.9.0"]])
+                 [org.clojure/test.check "0.9.0"]]
+  :profiles {:dev {:plugins [[lein-eftest "0.4.3"]
+                             [lein-auto "0.1.3"]]}})
